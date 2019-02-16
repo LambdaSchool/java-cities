414051c21f2ebb0027a5f9baadc2211575a7ad71
Derrick Mei <derrick.mei.168@gmail.com>
2019-02-16 02:29:30 -0600

    created: CityMessageListener class

---
+++ src/main/java/com/lambdaschool/javacities/AfCityMessageListener.java
@@ -0,0 +1,29 @@
+package com.lambdaschool.javacities;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+// Spring will run this in the background
+@Service
+public class AfCityMessageListener
+{
+    @RabbitListener(queues = AaJavacitiesApplication.SECRET_QUEUE)
+    public void receiveSecretMessage(AeCityMessage scm)
+    {
+        log.info("Received Secret Message: {}", scm.toString());
+    }
+
+    @RabbitListener(queues = AaJavacitiesApplication.CITIES1)
+    public void receiveCities1Message(AeCityMessage c1cm)
+    {
+        log.info("Received Cities 1 Message: {}", c1cm.toString());
+    }
+
+    @RabbitListener(queues = AaJavacitiesApplication.CITIES2)
+    public void receiveCities2Message(AeCityMessage c2cm)
+    {
+        log.info("Received Cities 2 Message: {}", c2cm.toString());
+    }
+}
