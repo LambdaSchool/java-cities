7ed4db60a61c26d0be2845da5f5f23b6f0b4c493
Derrick Mei <derrick.mei.168@gmail.com>
2019-02-16 01:34:51 -0600

    Derrick-Mei
    Initial commit - created template

---
+++ src/main/java/com/lambdaschool/javacities/City.java
@@ -0,0 +1,5 @@
+package com.lambdaschool.javacities;
+
+public class City
+{
+}
---
+++ src/main/java/com/lambdaschool/javacities/JavacitiesApplication.java
@@ -0,0 +1,16 @@
+package com.lambdaschool.javacities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavacitiesApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(JavacitiesApplication.class, args);
+    }
+
+}
+
